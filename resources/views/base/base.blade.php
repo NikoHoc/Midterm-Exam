c75eacbda4e4256da83794915a9613fb7765de97
Nikolas Henrik Owen <niko.owen88@gmail.com>
2024-10-15 11:20:18 +0700

    install tailwind and daisy ui

---
+++ resources/views/base/base.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    @vite('resources/css/app.css')  
+    @yield('library-css')
+    <title>Document</title>
+</head>
+<body>
+    @include('includes.navbar')
+
+    <div class="w-full">
+        @yield('content')
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+
+    @vite(['resources/js/app.js'])
+    @yield('library-js')
+</body>
+</html>
---
+++ resources/views/flights/index.blade.php
@@ -0,0 +1,8 @@
+@extends('base.base')
+
+@section('content')
+
+<h1>Hello World from flighst</h1>
+
+
+@endsection
