846f2a327242c0adad093624e3b6d570598c4157
Nikolas Henrik Owen <niko.owen88@gmail.com>
2024-10-15 13:58:12 +0700

    fixed footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,16 +1,13 @@
-<footer class="footer footer-center bg-primary text-primary-content p-5  bottom-0">
+<footer class="footer footer-center bg-primary text-primary-content p-3 absolute bottom-0">
     <aside>
-      <svg xmlns="http://www.w3.org/2000/svg" width="5em" height="5em" viewBox="0 0 36 36"><path fill="currentColor" d="M6.25 11.5L12 13.16l6.32-4.59l-9.07.26a.5.5 0 0 0-.25.08l-2.87 1.65a.51.51 0 0 0 .12.94" class="clr-i-solid clr-i-solid-path-1"/><path fill="currentColor" d="M34.52 6.36L28.22 5a3.78 3.78 0 0 0-3.07.67L6.12 19.5l-4.57-.2a1.25 1.25 0 0 0-.83 2.22l4.45 3.53a.55.55 0 0 0 .53.09c1.27-.49 6-3 11.59-6.07l1.12 11.51a.55.55 0 0 0 .9.37l2.5-2.08a.76.76 0 0 0 .26-.45l2.37-13.29c4-2.22 7.82-4.37 10.51-5.89a1.55 1.55 0 0 0-.43-2.88" class="clr-i-solid clr-i-solid-path-2"/><path fill="none" d="M0 0h36v36H0z"/></svg>
       <p class="font-bold">
         Web Framework and Deployment Midterm Exam
         <br />
         Airplane Booking System
       </p>
       <p>By C14220293 - Nikolas Henrik Owen </p>
-    </aside>
-    <nav>
       <a href="https://github.com/NikoHoc/Midterm-Exam">
         <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 16 16"><path fill="currentColor" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59c.4.07.55-.17.55-.38c0-.19-.01-.82-.01-1.49c-2.01.37-2.53-.49-2.69-.94c-.09-.23-.48-.94-.82-1.13c-.28-.15-.68-.52-.01-.53c.63-.01 1.08.58 1.23.82c.72 1.21 1.87.87 2.33.66c.07-.52.28-.87.51-1.07c-1.78-.2-3.64-.89-3.64-3.95c0-.87.31-1.59.82-2.15c-.08-.2-.36-1.02.08-2.12c0 0 .67-.21 2.2.82c.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82c.44 1.1.16 1.92.08 2.12c.51.56.82 1.27.82 2.15c0 3.07-1.87 3.75-3.65 3.95c.29.25.54.73.54 1.48c0 1.07-.01 1.93-.01 2.2c0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/></svg>
       </a>
-    </nav>
+    </aside>
   </footer>
